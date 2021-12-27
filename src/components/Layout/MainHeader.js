3c6460af9561809fc4cdd3651b3b4e77a27ae3ff
Natan <nano888@interia.eu>
2021-12-27 22:22:35 +0100

    Add a logout button to the menu

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,13 +1,23 @@
+import { useSelector, useDispatch } from 'react-redux'; 
+import { authActions } from '../../store';
+
 import classes from './MainHeader.module.css';
 
 const MainHeader = (props) => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+  const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
+
   return (
     <header className={classes.Header}>
       <h1>Recruitment Project</h1>
       <nav>
         <ul>
           <li>
-            {/* <button>Login</button> */}
+            {isAuthenticated && <button onClick={logoutHandler}>Logout</button> }
           </li>
         </ul>
       </nav>
